f7713c6b94cfaf9d06becc23e7850968d2bf0c90
guldmann <john@pcis.se>
2016-05-27 09:34:42 +0200

    Serverlist
    Some work and testing on server list

--- UniSearch/UniSearch/Form1.Designer.cs
+++ UniSearch/UniSearch/Form1.Designer.cs
@@ -47,35 +47,40 @@
             this.vbnmvbmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBoxServerList = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.buttonNewServer = new System.Windows.Forms.Button();
             this.buttonEditServer = new System.Windows.Forms.Button();
+            this.buttonServerListDelete = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(3, 563);
+            this.richTextBox1.Location = new System.Drawing.Point(4, 880);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(845, 362);
+            this.richTextBox1.Size = new System.Drawing.Size(1266, 563);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(9, 25);
+            this.textBoxName.Location = new System.Drawing.Point(14, 39);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(298, 22);
+            this.textBoxName.Size = new System.Drawing.Size(445, 31);
             this.textBoxName.TabIndex = 4;
             this.textBoxName.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Search_KeyUpEnter);
             // 
             // textBoxExtension
             // 
-            this.textBoxExtension.Location = new System.Drawing.Point(311, 25);
+            this.textBoxExtension.Location = new System.Drawing.Point(466, 39);
+            this.textBoxExtension.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxExtension.Name = "textBoxExtension";
-            this.textBoxExtension.Size = new System.Drawing.Size(90, 22);
+            this.textBoxExtension.Size = new System.Drawing.Size(133, 31);
             this.textBoxExtension.TabIndex = 5;
             this.textBoxExtension.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Search_KeyUpEnter);
             // 
@@ -85,9 +90,11 @@
             this.groupBox1.Controls.Add(this.labelSearchTime);
             this.groupBox1.Controls.Add(this.labelFilesValue);
             this.groupBox1.Controls.Add(this.labelFiles);
-            this.groupBox1.Location = new System.Drawing.Point(485, 12);
+            this.groupBox1.Location = new System.Drawing.Point(728, 19);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(395, 78);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(592, 122);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Index";
@@ -95,36 +102,40 @@
             // labelSearchTimeValue
             // 
             this.labelSearchTimeValue.AutoSize = true;
-            this.labelSearchTimeValue.Location = new System.Drawing.Point(181, 50);
+            this.labelSearchTimeValue.Location = new System.Drawing.Point(272, 78);
+            this.labelSearchTimeValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelSearchTimeValue.Name = "labelSearchTimeValue";
-            this.labelSearchTimeValue.Size = new System.Drawing.Size(16, 17);
+            this.labelSearchTimeValue.Size = new System.Drawing.Size(24, 25);
             this.labelSearchTimeValue.TabIndex = 3;
             this.labelSearchTimeValue.Text = "0";
             // 
             // labelSearchTime
             // 
             this.labelSearchTime.AutoSize = true;
-            this.labelSearchTime.Location = new System.Drawing.Point(83, 50);
+            this.labelSearchTime.Location = new System.Drawing.Point(124, 78);
+            this.labelSearchTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelSearchTime.Name = "labelSearchTime";
-            this.labelSearchTime.Size = new System.Drawing.Size(92, 17);
+            this.labelSearchTime.Size = new System.Drawing.Size(139, 25);
             this.labelSearchTime.TabIndex = 2;
             this.labelSearchTime.Text = "Search Time:";
             // 
             // labelFilesValue
             // 
             this.labelFilesValue.AutoSize = true;
-            this.labelFilesValue.Location = new System.Drawing.Point(181, 22);
+            this.labelFilesValue.Location = new System.Drawing.Point(272, 34);
+            this.labelFilesValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelFilesValue.Name = "labelFilesValue";
-            this.labelFilesValue.Size = new System.Drawing.Size(16, 17);
+            this.labelFilesValue.Size = new System.Drawing.Size(24, 25);
             this.labelFilesValue.TabIndex = 1;
             this.labelFilesValue.Text = "0";
             // 
             // labelFiles
             // 
             this.labelFiles.AutoSize = true;
-            this.labelFiles.Location = new System.Drawing.Point(41, 22);
+            this.labelFiles.Location = new System.Drawing.Point(62, 34);
+            this.labelFiles.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelFiles.Name = "labelFiles";
-            this.labelFiles.Size = new System.Drawing.Size(134, 17);
+            this.labelFiles.Size = new System.Drawing.Size(204, 25);
             this.labelFiles.TabIndex = 0;
             this.labelFiles.Text = "Search found Files: ";
             // 
@@ -137,10 +148,11 @@
             this.listViewResult.FullRowSelect = true;
             this.listViewResult.GridLines = true;
             this.listViewResult.HoverSelection = true;
-            this.listViewResult.Location = new System.Drawing.Point(7, 106);
+            this.listViewResult.Location = new System.Drawing.Point(-11, 284);
+            this.listViewResult.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listViewResult.MultiSelect = false;
             this.listViewResult.Name = "listViewResult";
-            this.listViewResult.Size = new System.Drawing.Size(872, 460);
+            this.listViewResult.Size = new System.Drawing.Size(1306, 716);
             this.listViewResult.TabIndex = 10;
             this.listViewResult.UseCompatibleStateImageBehavior = false;
             this.listViewResult.View = System.Windows.Forms.View.Details;
@@ -165,9 +177,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(405, 22);
+            this.button1.Location = new System.Drawing.Point(608, 34);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 36);
             this.button1.TabIndex = 11;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
@@ -182,66 +195,61 @@
             this.contextMenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.contextMenuStrip1.Size = new System.Drawing.Size(145, 52);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(224, 80);
             // 
             // testToolStripMenuItem
             // 
             this.testToolStripMenuItem.Name = "testToolStripMenuItem";
-            this.testToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(223, 38);
             this.testToolStripMenuItem.Text = "test";
             // 
             // vbnmvbmToolStripMenuItem
             // 
             this.vbnmvbmToolStripMenuItem.Name = "vbnmvbmToolStripMenuItem";
-            this.vbnmvbmToolStripMenuItem.Size = new System.Drawing.Size(144, 24);
+            this.vbnmvbmToolStripMenuItem.Size = new System.Drawing.Size(223, 38);
             this.vbnmvbmToolStripMenuItem.Text = "vbnmvbm";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 6);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(8, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.Size = new System.Drawing.Size(112, 25);
             this.label1.TabIndex = 12;
             this.label1.Text = "File name:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(307, 6);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(460, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.Size = new System.Drawing.Size(113, 25);
             this.label2.TabIndex = 13;
             this.label2.Text = "Extension:";
             // 
-            // listBox1
+            // listBoxServerList
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(9, 82);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(213, 20);
-            this.listBox1.TabIndex = 14;
+            this.listBoxServerList.FormattingEnabled = true;
+            this.listBoxServerList.ItemHeight = 25;
+            this.listBoxServerList.Location = new System.Drawing.Point(14, 103);
+            this.listBoxServerList.Name = "listBoxServerList";
+            this.listBoxServerList.Size = new System.Drawing.Size(318, 54);
+            this.listBoxServerList.TabIndex = 14;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 62);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 75);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.Size = new System.Drawing.Size(133, 25);
             this.label3.TabIndex = 15;
             this.label3.Text = "Index Server";
             // 
             // buttonNewServer
             // 
-            this.buttonNewServer.Location = new System.Drawing.Point(226, 74);
-            this.buttonNewServer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonNewServer.Location = new System.Drawing.Point(339, 116);
             this.buttonNewServer.Name = "buttonNewServer";
-            this.buttonNewServer.Size = new System.Drawing.Size(50, 27);
+            this.buttonNewServer.Size = new System.Drawing.Size(75, 42);
             this.buttonNewServer.TabIndex = 16;
             this.buttonNewServer.Text = "New";
             this.buttonNewServer.UseVisualStyleBackColor = true;
@@ -249,23 +257,42 @@
             // 
             // buttonEditServer
             // 
-            this.buttonEditServer.Location = new System.Drawing.Point(280, 74);
-            this.buttonEditServer.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonEditServer.Location = new System.Drawing.Point(420, 116);
             this.buttonEditServer.Name = "buttonEditServer";
-            this.buttonEditServer.Size = new System.Drawing.Size(50, 27);
+            this.buttonEditServer.Size = new System.Drawing.Size(75, 42);
             this.buttonEditServer.TabIndex = 17;
             this.buttonEditServer.Text = "Edit";
             this.buttonEditServer.UseVisualStyleBackColor = true;
             // 
+            // buttonServerListDelete
+            // 
+            this.buttonServerListDelete.Location = new System.Drawing.Point(516, 116);
+            this.buttonServerListDelete.Name = "buttonServerListDelete";
+            this.buttonServerListDelete.Size = new System.Drawing.Size(75, 40);
+            this.buttonServerListDelete.TabIndex = 18;
+            this.buttonServerListDelete.Text = "Delete";
+            this.buttonServerListDelete.UseVisualStyleBackColor = true;
+            this.buttonServerListDelete.Click += new System.EventHandler(this.buttonServerListDelete_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 173);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 33);
+            this.comboBox1.TabIndex = 19;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 916);
+            this.ClientSize = new System.Drawing.Size(1332, 1431);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonServerListDelete);
             this.Controls.Add(this.buttonEditServer);
             this.Controls.Add(this.buttonNewServer);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBoxServerList);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -274,6 +301,7 @@
             this.Controls.Add(this.textBoxExtension);
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "UniSearch";
             this.Activated += new System.EventHandler(this.Form1_Activated);
@@ -305,10 +333,12 @@
         private System.Windows.Forms.ToolStripMenuItem vbnmvbmToolStripMenuItem;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBoxServerList;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttonNewServer;
         private System.Windows.Forms.Button buttonEditServer;
+        private System.Windows.Forms.Button buttonServerListDelete;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- UniSearch/UniSearch/Form1.cs
+++ UniSearch/UniSearch/Form1.cs
@@ -18,33 +18,70 @@ namespace UniSearch
 {
     public partial class Form1 : Form
     {
-        private List<Server> ServerList; 
-     
+        private List<Server> ServerList = new List<Server>();
+
         public Form1()
         {
             InitializeComponent();
-            
+
+            //TODO save user (server) passwords in a password protected cryptated file ? 
             OpenServerList();
+            UpdateGuiServerList();
+
+
         }
 
-        private void OpenServerList()
+        
+
+        private void UpdateGuiServerList()
         {
+            //comboBox1.Items.Clear();
+            comboBox1.DataSource = ServerList;
+            comboBox1.DisplayMember = "name";
+            comboBox1.ValueMember = "name";
+
+
+            listBoxServerList.Items.Clear();
+            if (ServerList.Count < 1)
+            {
+                listBoxServerList.Items.Add("Add a Server");
+                listBoxServerList.Text = "Add a server ";
+            }
+            if (ServerList.Count > 1)
+            {
+                listBoxServerList.Items.Add("All servers");
+            }
+            if (ServerList.Count >= 1)
+            {
+                foreach (var server in ServerList)
+                {
+                    listBoxServerList.Items.Add(server.name);
+                }
+            }
+        }
+
+        private bool OpenServerList()
+        {
+            //TODO Add function to password protect file 
+            //And function to use password to open file 
             string ServerFile = "serverFile.bin";
             if (File.Exists(ServerFile))
             {
                 IFormatter formatter = new BinaryFormatter();
                 Stream stream = new FileStream(ServerFile, FileMode.Open, FileAccess.Read, FileShare.Read);
-                ServerList.AddRange((List<Server>)formatter.Deserialize(stream));
+                ServerList.AddRange((List<Server>) formatter.Deserialize(stream));
                 stream.Close();
+                return true;
             }
+            return false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-          
+
         }
-        
-       private void Search_KeyUpEnter(object sender, KeyEventArgs e)
+
+        private void Search_KeyUpEnter(object sender, KeyEventArgs e)
         {
             if (e.KeyData == Keys.Enter)
             {
@@ -86,7 +123,6 @@ namespace UniSearch
             proxy.ClientCredentials.UserName.Password = server.passWord;
 
             proxy.Endpoint.Address = new EndpointAddress(new Uri(server.url));
-            
             try
             {
                 proxy.Open();
@@ -95,40 +131,47 @@ namespace UniSearch
                     MessageBox.Show("Server is not ready");
                 }
                 else
-                { 
-                    var startTime = DateTime.Now; 
+                {
+                    var startTime = DateTime.Now;
                     var result = proxy.Search(textBoxName.Text, textBoxExtension.Text);
-                    List<IndexServiceReference.IndexFile> lst = result.OfType<IndexServiceReference.IndexFile>().ToList();
+                    List<IndexServiceReference.IndexFile> lst =
+                        result.OfType<IndexServiceReference.IndexFile>().ToList();
                     var resultTime = (DateTime.Now - startTime);
                     labelSearchTimeValue.Text = resultTime.TotalMilliseconds + "millisec";
                     PopulateListView(lst);
-                    labelFilesValue.Text = lst.Count.ToString(); 
-                }               
+                    labelFilesValue.Text = lst.Count.ToString();
+                }
+
                 proxy.Close();
             }
             catch (Exception ex)
             {
+
                 MessageBox.Show("Connection failed: " + ex.Message);
-            }           
+            }
         }
 
         private void PopulateListView(List<IndexServiceReference.IndexFile> resultList)
-        {                          
-			InitListView();
-			foreach (var indexFile in resultList)
-			{
-			   
-				listViewResult.UseWaitCursor = true; 
-				listViewResult.BeginUpdate();
-				var item = new ListViewItem(new string[] {"", indexFile.Folderk__BackingField, indexFile.Namek__BackingField, indexFile.Extensionk__BackingField});
-				listViewResult.Items.Add(item);
-				listViewResult.EndUpdate();
-				
-				listViewResult.UseWaitCursor = false; 
-			}           
+        {
+            InitListView();
+            foreach (var indexFile in resultList)
+            {
+                listViewResult.UseWaitCursor = true;
+                listViewResult.BeginUpdate();
+                var item =
+                    new ListViewItem(new string[]
+                    {
+                        "", indexFile.Folderk__BackingField, indexFile.Namek__BackingField,
+                        indexFile.Extensionk__BackingField
+                    });
+                listViewResult.Items.Add(item);
+                listViewResult.EndUpdate();
+
+                listViewResult.UseWaitCursor = false;
+            }
         }
 
-        private void InitListView()
+    private void InitListView()
         {
             listViewResult.Clear();
             listViewResult.View = View.Details;
@@ -161,16 +204,48 @@ namespace UniSearch
             Process.Start(listViewResult.SelectedItems[0].SubItems[1].Text + "\\" + listViewResult.SelectedItems[0].SubItems[2].Text);
         }
 
+        private bool SaveServerList()
+        {
+            string ServerFile = "serverFile.bin";
+            if (File.Exists(ServerFile))
+            {
+                File.Delete(ServerFile);
+            }
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+                Stream stream = new FileStream(ServerFile, FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, ServerList);
+                stream.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+
         private void buttonNewServer_Click(object sender, EventArgs e)
         {
-            Server server = new Server();
-            FromAddServer fas = new FromAddServer(server);
+            FromAddServer fas = new FromAddServer();
             DialogResult result = fas.ShowDialog();
             if (result == DialogResult.OK)
             {
-                //TODO get server object and save adn use it  !!...  
+                ServerList.Add(fas.NewServer);
+                SaveServerList();
+                UpdateGuiServerList();
             }
+            fas.Dispose();
+        }
+
+        private void buttonServerListDelete_Click(object sender, EventArgs e)
+        {
+            ServerList.RemoveAll(x => x.name == listBoxServerList.SelectedItem.ToString());
+            
+            SaveServerList();
+            UpdateGuiServerList();
         }
     }
-    
 }
